ac25f6153cf499da690998e9ed923ed9b62aa52b
mathi141299-cmyk <mathi141299@gmail.com>
2025-10-27 12:36:31 +0530

    Add files via upload

---
+++ src/assets/icons/index.tsx
@@ -0,0 +1,160 @@
+import { ReactComponent as LogInIcon } from "./Login.svg";
+import { ReactComponent as LogoIcon } from "./Logo.svg";
+import { ReactComponent as HidePasswordIcon } from "./HidePasswordIcon.svg";
+import { ReactComponent as ShowPasswordIcon } from "./ShowPasswordIcon.svg";
+import { ReactComponent as HighlightedRestaurantIcon } from "./HighlightedRestaurantIcon.svg";
+import { ReactComponent as AdminIcon } from "./AdminIcon.svg";
+import { ReactComponent as NavLogIcon } from "./NavLogIcon.svg";
+import { ReactComponent as NavListIcon } from "./NavListIcon.svg";
+import { ReactComponent as LocationIcon } from "./LocationIcon.svg";
+import { ReactComponent as AvatarIcon } from "./AvatarIcon.svg";
+import { ReactComponent as PasswordResetIcon } from "./PasswordResetIcon.svg";
+import { ReactComponent as LogOutIcon } from "./LogOutIcon.svg";
+import { ReactComponent as AdminPicIcon } from "./AdminPicIcon.svg";
+import { ReactComponent as RestaurantPicIcon } from "./RestaurantPicIcon.svg";
+import { ReactComponent as EqualIcon } from "./EqualIcon.svg";
+import { ReactComponent as MultipleIcon } from "./MultipleIcon.svg";
+import { ReactComponent as PrintIcon } from "./PrintIcon.svg";
+import { ReactComponent as RestaurantIcon } from "./RestaurantIcon.svg";
+import { ReactComponent as MedicalCampIcon } from "./MedicalCampIcon.svg";
+import { ReactComponent as ClinicIcon } from "./ClinicIcon.svg";
+import { ReactComponent as SpiritualTourIcon } from "./SpiritualTourIcon.svg";
+import { ReactComponent as PregnancyClassesIcon } from "./PregnancyClassesIcon.svg";
+import { ReactComponent as StudentCourseIcon } from "./StudentCourseIcon.svg";
+import { ReactComponent as HighLightedAdminIcon } from "./HighLightedAdminIcon.svg";
+import { ReactComponent as DeleteIcon } from "./DeleteIcon.svg";
+import { ReactComponent as EditIcon } from "./EditIcon.svg";
+import { ReactComponent as ArrowBackIcon } from "./ArrowBackIcon.svg";
+import { ReactComponent as ArrowFrontIcon } from "./ArrowFrontIcon.svg";
+import { ReactComponent as FilterIcon } from "./FilterIcon.svg";
+import { ReactComponent as ArrowUpIcon } from "./ArrowUpIcon.svg";
+import { ReactComponent as ArrowDownIcon } from "./ArrowDownIcon.svg";
+import { ReactComponent as SelectedUpArrow } from "./SelectedUpArrow.svg";
+import { ReactComponent as SelectedDownArrow } from "./SelectedDownArrow.svg";
+import { ReactComponent as ErrorIcon } from "./ErrorIcon.svg";
+import { ReactComponent as InfoIcon } from "./InfoIcon.svg";
+import { ReactComponent as SuccessIcon } from "./SuccessIcon.svg";
+import { ReactComponent as WarningIcon } from "./WarningIcon.svg";
+import { ReactComponent as AramPrintIcon } from "./AramPrintIcon.svg";
+import { ReactComponent as PrintDividerIcon } from "./PrintDividerIcon.svg";
+import { ReactComponent as HighlightedMedicalCampIcon } from "./HighlightedMedicalCampIcon.svg";
+import { ReactComponent as HighlightedClinicIcon } from "./HighlightedClinicIcon.svg";
+import { ReactComponent as HighlightedSpiritualTourIcon } from "./HighlightedSpiritualTourIcon.svg";
+import { ReactComponent as HighlightedPregnancyClassesIcon } from "./HighlightedPregnancyClassesIcon.svg";
+import { ReactComponent as HighlightedStudentCourseIcon } from "./HighlightedStudentCourseIcon.svg";
+// import Favicon from "./Favicon.png";
+import { ReactComponent as NotSatisfiedIcon } from "./NotSatisfied.svg";
+import { ReactComponent as ViewIcon } from "./ViewIcon.svg";
+import { ReactComponent as TablePrintIcon } from "./TablePrintIcon.svg";
+
+import { ReactComponent as FormLogo } from "./FormLogo.svg";
+import { ReactComponent as FormTitle } from "./FormTitle.svg";
+import { ReactComponent as PrintTableIcon } from "./PrintTableIcon.svg";
+// import { ReactComponent as PrintFormHeader } from "./PrintFormHeader.svg";
+import { ReactComponent as FormWaterMark } from "./FormWaterMark.svg";
+import { ReactComponent as ViewPrintIcon } from "./ViewPrintIcon.svg";
+import { ReactComponent as ExcelIcon } from "./ExcelIcon.svg";
+import { ReactComponent as FeedbackIcon } from "./Feedback.svg";
+import { ReactComponent as HighlightedFeedbackIcon } from "./HighlightedFeedbackIcon.svg";
+import { ReactComponent as HistoryIcon } from "./History.svg";
+import { ReactComponent as DashboardIcon } from "./DashBoard.svg";
+import { ReactComponent as HighLightedDashboardIcon } from "./HighLightedDashboardIcon.svg";
+import { ReactComponent as DeleteAlertIcon } from "./DeleteAlert.svg";
+import { ReactComponent as AdjustmentIcon } from "./AdjustmentIcon.svg";
+import { ReactComponent as DonationsIcon } from "./DonationsIcon.svg";
+import { ReactComponent as ExpensesIcon } from "./ExpensesIcon.svg";
+import { ReactComponent as NeetIcon } from "./NeetIcon.svg";
+import { ReactComponent as OthersIcon } from "./OthersIcon.svg";
+import { ReactComponent as ProfitAndLossIcon } from "./ProfitAndLossIcon.svg";
+import { ReactComponent as HighlightedAdjustmentIcon } from "./HighlightedAdjustmentIcon.svg";
+import { ReactComponent as HighlightedDonationsIcon } from "./HighlightedDonationsIcon.svg";
+import { ReactComponent as HighlightedExpensesIcon } from "./HighlightedExpensesIcon.svg";
+import { ReactComponent as HighlightedNeetIcon } from "./HighlightedNeetIcon.svg";
+import { ReactComponent as HighlightedOthersIcon } from "./HighlightedOthersIcon.svg";
+import { ReactComponent as HighlightedProfitAndLossIcon } from "./HighlightedProfitAndLossIcon.svg";
+import Adjustment from "./Adjustment.png";
+import HighlightedAdjustment from "./HighlightedAdjustment.png";
+import Others from "./Others.png";
+import HighlightedOthers from "./HighlightedOthers.png";
+import ProfitAndLoss from "./ProfitAndLoss.png";
+import HighlightedProfitAndLoss from "./HighlightedProfitAndLoss.png";
+
+export {
+  ViewIcon,
+  TablePrintIcon,
+  PrintDividerIcon,
+  AramPrintIcon,
+  LogInIcon,
+  LogoIcon,
+  HidePasswordIcon,
+  ShowPasswordIcon,
+  HighlightedRestaurantIcon,
+  AdminIcon,
+  NavLogIcon,
+  NavListIcon,
+  LocationIcon,
+  AvatarIcon,
+  PasswordResetIcon,
+  LogOutIcon,
+  AdminPicIcon,
+  RestaurantPicIcon,
+  EqualIcon,
+  MultipleIcon,
+  PrintIcon,
+  RestaurantIcon,
+  MedicalCampIcon,
+  ClinicIcon,
+  SpiritualTourIcon,
+  PregnancyClassesIcon,
+  StudentCourseIcon,
+  HighLightedAdminIcon,
+  EditIcon,
+  DeleteIcon,
+  ArrowFrontIcon,
+  ArrowBackIcon,
+  FilterIcon,
+  ArrowUpIcon,
+  ArrowDownIcon,
+  SelectedDownArrow,
+  SelectedUpArrow,
+  ErrorIcon,
+  InfoIcon,
+  SuccessIcon,
+  WarningIcon,
+  HighlightedMedicalCampIcon,
+  HighlightedPregnancyClassesIcon,
+  HighlightedSpiritualTourIcon,
+  HighlightedClinicIcon,
+  HighlightedStudentCourseIcon,
+  NotSatisfiedIcon,
+  FormTitle,
+  FormLogo,
+  PrintTableIcon,
+  FormWaterMark,
+  ViewPrintIcon,
+  ExcelIcon,
+  FeedbackIcon,
+  HighlightedFeedbackIcon,
+  HistoryIcon,
+  DashboardIcon,
+  HighLightedDashboardIcon,
+  DeleteAlertIcon,
+  AdjustmentIcon,
+  DonationsIcon,
+  ExpensesIcon,
+  NeetIcon,
+  OthersIcon,
+  ProfitAndLossIcon,
+  HighlightedAdjustmentIcon,
+  HighlightedDonationsIcon,
+  HighlightedExpensesIcon,
+  HighlightedNeetIcon,
+  HighlightedOthersIcon,
+  HighlightedProfitAndLossIcon,
+  Adjustment,
+  HighlightedAdjustment,
+  Others,
+  HighlightedOthers,
+  HighlightedProfitAndLoss,
+  ProfitAndLoss,
+};
---
+++ src/assets/images/index.tsx
@@ -0,0 +1,21 @@
+import PrintFormHeader from "./PrintFormHeader.png";
+import printDesignLogo from "./printDesignLogo.png";
+import printTextLogo from "./printTextLogo.png";
+import blackUnderline from "./blackUnderline.png";
+import grayUnderline from "./grayUnderline.png";
+import printDesignColorLogo from "./printDesignColorLogo.png";
+import printTextColorLogo from "./printTextColorLogo.png";
+import lightBlueUnderline from "./lightBlueUnderline.png";
+import lightGreenUnderline from "./lightGreenUnderline.png";
+
+export {
+  PrintFormHeader,
+  printDesignLogo,
+  printTextLogo,
+  blackUnderline,
+  grayUnderline,
+  printDesignColorLogo,
+  printTextColorLogo,
+  lightBlueUnderline,
+  lightGreenUnderline,
+};
